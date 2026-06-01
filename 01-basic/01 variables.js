627331f143ee95bcd6fbbd13e3363104a964f6d0
adityashri15 <softproaditya15@gmail.com>
2026-06-01 18:40:03 +0000

    config check

---
+++ 01-basic/01 variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="aditya@gmail.com"
+var accountPassword="123456"
+let accountstate;
+accountCity="jaipur"
+//accountId=1
+accountEmail="a@123"
+accountPassword="hi"
+accountCity="banglore"
+//dont use var because of scope issue..
+ //console.log(accountId)// copntant cant be changed
+ console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])
