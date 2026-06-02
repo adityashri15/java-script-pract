2149dfdf42e801031292bf0916ab6b08c0d783dd
adityashri15 <softproaditya15@gmail.com>
2026-06-02 06:11:20 +0000

    data type

---
+++ 01-basic/02  datatypes.js
@@ -0,0 +1,14 @@
+//string=>""
+let name = "John Doe";
+//number=> 1, 2, 3, 4.5
+let age = 30;
+//boolean=> true, false
+let isloggedIn = true;
+//undefined=> variable declared but not assigned any value
+let address;
+//null=> it is standalone value and returs object
+let city = null;
+//symbol=> it is unique and immutable data type
+let id = Symbol("id");
+//bigint=> it is used to represent large integers
+let bigNumber = 1234567890123456789012345678901234567890n;
