9a2b5ad986d6f339750b65ec224eb2123857bc0e
adityashri15 <softproaditya15@gmail.com>
2026-06-02 07:08:39 +0000

    conversion of datatypes

---
+++ 01-basic/03_conversionoperation.js
@@ -0,0 +1,36 @@
+// number to string
+let number=12;
+let stringNumber=String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+// string to number
+let string="1234abc"   //but if we write string="1234abc" then it will give NaN but it will convert the string to number type of data type
+let numberString=Number(string)
+console.log(numberString)
+console.log(typeof numberString)
+//boolean to string
+let isValue=true;
+let stringValue=String(isValue)
+console.log(stringValue)
+console.log(typeof stringValue)
+//string to boolean
+let stringvalue="true"
+let booleanValue=Boolean(stringvalue)
+console.log(booleanValue)
+console.log(typeof booleanValue)
+//nuber to boolean
+let num=1;
+let booleanNum=Boolean(num)
+console.log(booleanNum)
+console.log(typeof booleanNum)
+//boolean to number
+let bool=true;
+let numBool=Number(bool)
+console.log(numBool)
+console.log(typeof numBool)
+//null to string
+let nullValue=null;
+let stringNull=String(nullValue)
+console.log(stringNull)
+console.log(typeof stringNull)
+
