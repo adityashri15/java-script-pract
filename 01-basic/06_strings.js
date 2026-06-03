7cf6ab2fdc7170a9635dcfa22d9d91735c8cdfa8
adityashri15 <softproaditya15@gmail.com>
2026-06-03 08:47:53 +0000

    string manupulation

---
+++ 01-basic/06_strings.js
@@ -0,0 +1,26 @@
+// const name= "aditya"
+// const repocount=50
+// console.log(name + repocount +"value");
+// console.log(`my name is ${name} and my repo count is ${repocount}`)
+ let onename= new String("aditya-shrivastwa")
+//  console.log(onename.length)
+//  console.log(onename[1])
+//  console.log(onename.toUpperCase())
+//     console.log(onename.toLowerCase())
+//     console.log(onename.includes("shrivastwa"))
+//     console.log(onename.startsWith("aditya"))
+//     console.log(onename.endsWith("shrivastwa"))
+//     console.log(onename.indexOf("shrivastwa"))
+//     console.log(onename.split("shrivastwa"))
+//     console.log(onename.replace("aditya","aditya shrivastwa"))
+
+//     console.log(onename.trim("aditya shrivastwa")) 
+// // console.log(onename.substring(0,5))
+// // console.log(onename.slice(-1,5))
+// console.log(onename.charAt(0))
+// console.log(onename.charCodeAt(0)) 
+const url="https://www.youtube.com/channel/UCwfaAHy4zQUb2APNOGXUCDQ" 
+console.log(url.replace('channel','playlist'))
+console.log(onename.split(" "))
+console.log(onename.bold(2))
+
