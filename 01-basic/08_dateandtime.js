656000ae4a91e8b56f82a2e214278a2d622fc973
adityashri15 <softproaditya15@gmail.com>
2026-06-03 12:02:26 +0000

    date and time

---
+++ 01-basic/08_dateandtime.js
@@ -0,0 +1,26 @@
+//dates
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getMonth());
+// console.log(mydate.getDate());
+// console.log(mydate.getDay());
+// console.log(mydate.getHours());
+// console.log(mydate.getMinutes());
+// console.log(mydate.getSeconds());
+// console.log(mydate.getTime());
+// console.log(mydate.getTimezoneOffset());
+// console.log(mydate.getUTCDate());
+// console.log(mydate.getUTCDay());
+// console.log(mydate.getUTCFullYear());
+let newdate = new Date(2019,0,15,10,30,15)
+// 
+let myTimestamp = Date.now()
+// console.log(myTimestamp)
+// console.log(newdate.getTime())
+let date1 = new Date()
+console.log(date1)
